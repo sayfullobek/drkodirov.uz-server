995f2fe63b464d867a906207fa5d3ee5e9783ff8
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2025-05-10 01:48:27 -0700

    middlewares change update

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,7 +2,9 @@ const multer = require('multer')
 const path = require('path')
 
 const storage = multer.diskStorage({
-	destination: (req, file, cb) => cb(null, 'uploads/'),
+	destination: (req, file, cb) => {
+		cb(null, path.join(__dirname, '../../uploads'))
+	},
 	filename: (req, file, cb) => {
 		const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9) + path.extname(file.originalname)
 		cb(null, uniqueName)

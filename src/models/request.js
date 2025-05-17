2280c235cf418cad4a2cb8bc8524fcf7497ebf2f
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2025-05-17 02:26:48 -0400

    Update request.js

--- src/models/request.js
+++ src/models/request.js
@@ -1,19 +1,40 @@
-const { Schema, model } = require("mongoose")
-const { schemaOptions } = require("./modelOptions")
+const { Schema, model } = require("mongoose");
+const { schemaOptions } = require("./modelOptions");
+
 const requestSchema = new Schema({
 	fullName: {
 		type: String,
 		required: true,
-	}, phoneNumber: {
+	},
+	phoneNumber: {
 		type: String,
 		required: true,
-	}, date: {
+	},
+	date: {
 		type: String,
 		required: true,
-	}, message: {
+	},
+	message: {
 		type: String,
 		required: true,
 		maxlength: 20000
+	},
+	isFromUzbekistan: {
+		type: Boolean,
+		required: true,
+	},
+	region: {
+		type: String,
+		required: function () {
+			return this.isFromUzbekistan === true;
+		}
+	},
+	country: {
+		type: String,
+		required: function () {
+			return this.isFromUzbekistan === false;
+		}
 	}
-}, schemaOptions)
-module.exports = model('request', requestSchema)
+}, schemaOptions);
+
+module.exports = model('request', requestSchema);
